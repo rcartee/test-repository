6f8896fc479308c55df7b6a113a9c78ed27005a9
rcartee <rcartee@outlook.com>
2024-10-04 14:36:35 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main(int argc, char* args[])
 {
-    for (int i = 1; i < argc; i++)
+    for (int i = argc - 1; i > 0; i--)
     {
         std::cout << args[i] << std::endl;
     }
